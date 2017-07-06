6a61157350ed08942fca7b0b1930e2434881d324
gabegordon <gabegordon1234@gmail.com>
2017-07-06 16:13:05 -0400

    Fix OMPS decom bug. (headers offset)

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
         std::string instrument = argv[1];
         std::string packetFile = argv[2];
         std::string paramsFile = argv[3];
-        bool debug = false;
+        bool debug = true;
         bool allAPIDs = !!std::stoi(argv[4]);
         std::cout << packetFile << std::endl;
         system("cd output && del /Q *.txt 2>NUL 1>NUL");
