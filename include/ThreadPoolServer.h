89c9349ae263ce4b9aa9b551fdb3ffe1561d0bcd
gabegordon <gabegordon1234@gmail.com>
2017-07-07 15:01:20 -0400

    Add synchronization for multiple writer helper threads.

--- include/ThreadPoolServer.h
+++ include/ThreadPoolServer.h
@@ -12,17 +12,19 @@ class ThreadPoolServer
 public:
 	ThreadPoolServer(const std::string& instrument) :
 		m_queue(),
-		m_instrument(instrument)
+		m_instrument(instrument),
+    m_num_threads(4)
 	{}
 	~ThreadPoolServer() {}
 	void start();
-	void exec(const DataTypes::Packet pack);
+	void exec(DataTypes::Packet& pack);
   void join();
-  void ThreadMain(ThreadSafeListenerQueue<DataTypes::Packet>& queue, const std::string instrument, ThreadSafeStreamMap& outfiles);
+  void ThreadMain(ThreadSafeListenerQueue& queue, const std::string instrument, ThreadSafeStreamMap& outfiles);
 private:
-	ThreadSafeListenerQueue<DataTypes::Packet> m_queue;
-	std::thread m_thread;
+	ThreadSafeListenerQueue m_queue;
+	std::vector<std::thread> m_threads;
 	std::string m_instrument;
   ThreadSafeStreamMap m_outfiles;
+  uint32_t m_num_threads;
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,11 @@
 #include "Decom.h"
 #include "DatabaseReader.h"
 
-using std::cout;
-using std::endl;
 int main(int argc, char* argv[])
 {
     if (argc < 5)
     {
-        cout << "Specify: database and instrument\n";
+        std::cout << "Specify: database and instrument\n";
         return 0;
     }
     else
@@ -17,7 +15,7 @@ int main(int argc, char* argv[])
         std::string instrument = argv[1];
         std::string packetFile = argv[2];
         std::string paramsFile = argv[3];
-        bool debug = true;
+        bool debug = false;
         bool allAPIDs = !!std::stoi(argv[4]);
         std::cout << packetFile << std::endl;
         system("cd output && del /Q *.txt 2>NUL 1>NUL");
